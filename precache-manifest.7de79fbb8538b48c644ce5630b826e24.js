ae5c46509ebf8debd0ed7f20aafedd027fb69d94
Kaan Seyitogullari <kaan.seyitogullari@zalando.ie>
2019-12-30 16:22:19 +0000

    Updates

--- precache-manifest.7de79fbb8538b48c644ce5630b826e24.js
+++ precache-manifest.7de79fbb8538b48c644ce5630b826e24.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b28942c7b68dd6dab6b0e24d83b3a72",
+    "revision": "62d326f15e9a56c7a25c5f09db1eda82",
     "url": "/svg-charts-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/svg-charts-react/static/css/2.b4719924.chunk.css"
   },
   {
-    "revision": "367f6be3d01fcb67ac9e",
+    "revision": "8dd3b69571e531d79d86",
     "url": "/svg-charts-react/static/css/main.795991f2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/svg-charts-react/static/js/2.3c87b441.chunk.js"
   },
   {
-    "revision": "367f6be3d01fcb67ac9e",
-    "url": "/svg-charts-react/static/js/main.893e0dba.chunk.js"
+    "revision": "8dd3b69571e531d79d86",
+    "url": "/svg-charts-react/static/js/main.bf337b85.chunk.js"
   },
   {
     "revision": "6a2dcf80db0d2956381a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svg-charts-react/precache-manifest.cc272fdc01abf12d5e0f7bd152a35d76.js"
+  "/svg-charts-react/precache-manifest.7de79fbb8538b48c644ce5630b826e24.js"
 );
 
 self.addEventListener('message', (event) => {
