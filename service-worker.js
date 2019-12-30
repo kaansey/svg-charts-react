6af29fe9b360ed5ff9a8eedf3dfdede645b0873e
Kaan Seyitogullari <kaan.seyitogullari@zalando.ie>
2019-12-30 16:41:15 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svg-charts-react/precache-manifest.3aa0993a79fb97067e3df6bc3702d1c8.js"
+  "/svg-charts-react/precache-manifest.d9e16cfec6bc62b0cad6dad32398f1c4.js"
 );
 
 self.addEventListener('message', (event) => {
